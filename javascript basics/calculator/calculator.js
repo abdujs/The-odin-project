c63b6918929d8c1ac7030831432113bcce27b39e
abdujs <139302181+abdujs@users.noreply.github.com>
2024-03-19 14:41:49 +0300

    add function to comppute the resullt

--- javascript basics/calculator/calculator.js
+++ javascript basics/calculator/calculator.js
@@ -61,3 +61,33 @@ function chooseOperation(selectedOperation) {
   previousOperand = currentOperand;
   currentOperand = '';
 }
+
+//Computing the Result
+function compute() {
+  let computation;
+  const prev = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+  if (isNaN(prev) || isNaN(current)) return;
+ 
+  switch (operation) {
+      case '+':
+          computation = prev + current;
+          break;
+      case '-':
+          computation = prev - current;
+          break;
+      case '*':
+          computation = prev * current;
+          break;
+      case '/':
+          computation = prev / current;
+          break;
+      default:
+          return;
+  }
+
+  currentOperand = computation;
+  operation = undefined;
+  previousOperand = '';
+  updateDisplay(); // Refresh the display with the new state
+}
